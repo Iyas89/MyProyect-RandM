69f3e986ac7728585523735cfc94ebe60db289ef
iyas89 <iyasasaad@gmail.com>
2023-09-14 23:05:21 -0300

    front and back

---
+++ New folder/iyas-ram-vit/cliente/src/components/Favorites/Favorites.jsx
@@ -0,0 +1,64 @@
+import {useSelector, useDispatch} from "react-redux";
+import { useState } from "react";
+
+import Card from "../card/Card"; 
+import { orderCards, filterCards } from "../../redux/actions";
+
+
+
+const Favorites = () => {
+  const dispatch = useDispatch();
+  const myFavorites = useSelector((state) => state.myFavorites);
+
+  const [aux, setAux] = useState(false)
+const handleOrder = (event) => {
+  dispatch(orderCards(event.target.value));
+  setAux(true);
+}
+
+const handleFilter = (event) => {
+  dispatch(filterCards(event.target.value));
+}
+
+
+  return (
+    <div>
+      <select placeholder="Gender" onChange={handleFilter}>
+        {["", "Male", "Female", "unknown", "Genderless", "todos"].map((gender) => (
+          <option key={gender} value={gender}>
+            {gender}
+          </option>
+        ))}
+      </select>
+      <select placeholder="Sort" onChange={handleOrder}>
+        {["Ascendente", "Descendente"].map((order) => (
+          <option key={order} value={order}>
+            {order}
+          </option>
+        ))}
+      </select>
+      { 
+      myFavorites?.map(fav => {
+        return (
+          < Card
+          //species, gender, image
+          key={fav.id}
+          id={fav.id}
+          name={fav.name}
+          species={fav.species}
+          gender={fav.gender}
+          image={fav.image}
+          onClose={fav.onClose}
+          />
+        )
+      }
+        )
+      }
+    </div>
+    )
+
+  }
+  
+  
+
+  export default Favorites;
